94a4087503d3daa5a3ecc417f466ec3bb4138f8b
Faradey27 <andriit@wix.com>
2018-12-03 12:34:36 +0200

    Init project with stylable

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Root = () => {
+  return <div>Hello Ugly stylable bug</div>;
+};
+
+ReactDOM.render(<Root />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const StylableWebpackPlugin = require('@stylable/webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  context: __dirname,
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new StylableWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
+};
